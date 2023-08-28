ed66852fe8d318e94c3fb12a4657f77b7f22e347
bikramdhami0011 <bikramdhami296@gmail.com>
2023-08-28 15:13:15 +0545

    shop

---
+++ src/app/component/customer/getcustomer/customerbox/page.js
@@ -0,0 +1,17 @@
+
+
+const Customerbox = (props) => {
+ 
+  return (
+    <div>
+        <div>
+             <div>
+                 <h1>{props.cust.customer}</h1>
+                 <p> enter email</p>
+             </div>
+        </div>
+    </div>
+  )
+}
+
+export default Customerbox
