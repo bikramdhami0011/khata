a4850a3db8786b25075c7264556454e6c91c4a26
bikramdhami0011 <bikramdhami296@gmail.com>
2023-08-28 16:05:00 +0545

    hooo

--- src/dbcon/dbcon.js
+++ src/dbcon/dbcon.js
@@ -1,2 +1,2 @@
 const {name,password}=process.env;
-export const  Dbcon= "mongodb+srv://"+name+":"+password+"@cluster0.7zwsahn.mongodb.net/ContactUs?retryWrites=true&w=majority"
+export const  Dbcon= "mongodb+srv://bikramdhami296:bikramdhami@cluster0.7zwsahn.mongodb.net/ContactUs?retryWrites=true&w=majority"
